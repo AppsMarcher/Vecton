b0b670ed72fa11108c96df532ecfc53708654f7f
Ricardo da Silveira Guimarães <rguimaraes@marcher.local>
2026-08-27 10:58:37 -0300

    feat(comercial): adiciona dicionario ao relatorio de pecas

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = "vecton-static-";
-const CACHE_NAME = `${CACHE_PREFIX}20260827b`;
+const CACHE_NAME = `${CACHE_PREFIX}20260827c`;
 const APP_SHELL = [
   "./",
   "./index.html",
