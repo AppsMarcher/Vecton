917b5f77c38edbf885c7761f40ac24700e2e9be1
Ricardo da Silveira Guimarães <rguimaraes@marcher.local>
2026-08-31 09:35:52 -0300

    pwa: aviso de nova versao disponivel nunca quebra em 2 linhas

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
 const CACHE_PREFIX = "vecton-static-";
-const CACHE_NAME = `${CACHE_PREFIX}20260831b`;
+const CACHE_NAME = `${CACHE_PREFIX}20260831c`;
 const APP_SHELL = [
   "./",
   "./index.html",
   "./manifest.webmanifest",
-  "./styles.css?v=20260831f",
+  "./styles.css?v=20260831g",
   "./fav-icon.png",
   "./assets/msn-message.mp3?v=20260804b",
   "./assets/icq.mp3?v=20260804a",
