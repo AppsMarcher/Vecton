75c0879dea403f96b621abf2b6c1b59caa47d21f
Ricardo da Silveira Guimarães <rguimaraes@marcher.local>
2026-08-31 09:38:55 -0300

    fix: breadcrumb do painel mobile (.vmob-crumb) nunca tinha CSS, sobrepondo o titulo com aparencia nativa de botao

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
 const CACHE_PREFIX = "vecton-static-";
-const CACHE_NAME = `${CACHE_PREFIX}20260831c`;
+const CACHE_NAME = `${CACHE_PREFIX}20260831d`;
 const APP_SHELL = [
   "./",
   "./index.html",
   "./manifest.webmanifest",
-  "./styles.css?v=20260831g",
+  "./styles.css?v=20260831h",
   "./fav-icon.png",
   "./assets/msn-message.mp3?v=20260804b",
   "./assets/icq.mp3?v=20260804a",
