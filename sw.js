4d70575e2abb9c5a79479fbc162fe7c9dbead6b3
Ricardo da Silveira Guimarães <rguimaraes@marcher.local>
2026-08-31 23:31:51 -0300

    fix: restaura cabecalho do Messenger mobile

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
 const CACHE_PREFIX = "vecton-static-";
-const CACHE_NAME = `${CACHE_PREFIX}20260831zm`;
+const CACHE_NAME = `${CACHE_PREFIX}20260831zn`;
 const APP_SHELL = [
   "./",
   "./index.html",
   "./manifest.webmanifest",
-  "./styles.css?v=20260831zp",
+  "./styles.css?v=20260831zq",
   "./fav-icon.png",
   "./assets/msn-message.mp3?v=20260804b",
   "./assets/icq.mp3?v=20260804a",
