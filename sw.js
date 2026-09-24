9dc2b28ee19371209f19bac65599a6732024e7c7
Ricardo da Silveira Guimarães <rguimaraes@marcher.local>
2026-09-24 14:26:03 -0300

    fix(reports): prevent floating scrollbar from resetting horizontal position

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = "vecton-static-";
-const CACHE_NAME = `${CACHE_PREFIX}20260924maps2`;
+const CACHE_NAME = `${CACHE_PREFIX}20260924scroll1`;
 const APP_SHELL = [
   "./",
   "./index.html",
